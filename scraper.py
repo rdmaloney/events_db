0b2aec71da76cb9f7b4844f2a82948e72c07009d
rdmaloney <45108790+rdmaloney@users.noreply.github.com>
2019-08-06 18:43:32 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -42,10 +42,10 @@ def scrape_data():
             box_item = soup.find('ul',{'class': "b-list__box-list"})
             box_item = box_item.find_all('li')
 
-            place = box_item[1].text.strip()
+            place = box_item[1].text.strip().strip("Location:").strip()
             location.append(place)
 
-            d = box_item[0].text.strip()
+            d = box_item[0].text.strip().strip("Date:").strip()
             date.append(d)
 
             rows = soup.find_all('table', {"class": "b-fight-details__table b-fight-details__table_style_margin-top b-fight-details__table_type_event-details js-fight-table"})
